c623c7990e29cd7c79e28f69b361c58e9c2dd4e2
Anderson Campos <andersonbcamposbr@gmail.com>
2024-11-13 23:08:19 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    Pessoa newPessoa = new Pessoa("Maria", 45);
+
+    System.out.println("Nome: " + newPessoa.nome + ", idade: " + newPessoa.idade);
+    }
+}
